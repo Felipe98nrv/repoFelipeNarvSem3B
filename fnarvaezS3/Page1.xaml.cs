c890d66fc1238e717b10cbc2d388bb63449c919c
master <master@SMLAB108PC05_23>
2023-06-13 21:21:16 -0500

    Agregar archivos de proyecto.

---
+++ fnarvaezS3/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace fnarvaezS3
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string dato1=txtdatouno.Text;
+            double dato2=Convert.ToDouble(txtdatodos.Text);
+
+            Navigation.PushAsync(new Page2(dato1, dato2));
+
+        }
+    }
+}
